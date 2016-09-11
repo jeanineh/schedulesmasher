2ca1b9816ec6464a614639a378c83427531f897a
mogh <mogh@example.com>
2016-09-11 15:00:47 -0400

    using the icalendar gem

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,12 +1,13 @@
 #require 'calendar_events_helper.rb'
+require 'icalendar'
 
 class Meeting < ActiveRecord::Base
 
 	def self.read_file(filename)
 		contents = []
-		File.foreach(filename) do |line|
-			contents.append(line)
-		end
-		return contents
+		cal_file = File.open(filename)
+		events = Icalendar::Event.parse(cal_file)
+		event = events.first
+		return event
 	end
 end
