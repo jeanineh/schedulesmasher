d889031072ab2d05739d31b96e54e76298ed7ba4
Jennifer Han <jhan2@andrew.cmu.edu>
2016-09-19 18:14:45 -0400

    switched filtering from scopes in calendar event model to part of function in team members model

--- app/models/calendar_event.rb
+++ app/models/calendar_event.rb
@@ -3,8 +3,8 @@ require 'date'
 class CalendarEvent < ActiveRecord::Base
   belongs_to :team_member
 
-  scope :future, -> { where("end > ?", DateTime.now) }
-  scope :one_week_from_today, -> { where("end < ?", 7.days.from_now) }
+  # scope :future, -> { where("end > ?", DateTime.now) }
+  # scope :one_week_from_today, -> { where("end < ?", 7.days.from_now) }
 
   def to_date
     "#{self.start}, #{self.end}"
