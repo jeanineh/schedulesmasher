3474df472e39acfd4071d6ca53dbea915aad9356
mogh <mogh@example.com>
2016-09-25 16:05:12 -0700

    added tooltips!

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -6,12 +6,35 @@ class Meeting < ActiveRecord::Base
 		count = 0
 		team_members.each do |member|
 			member.calendar_events.each do |event|
-				#if(given_date.hour >= event.start.hour)&&(given_date.hour < event.end.hour)&&(given_date.day==event.start.day)
+				if(given_date.hour >= event.start.hour)&&(given_date.hour < event.end.hour)&&(given_date.day==event.start.day)
 				count = count+1
-				#end
+				end
 			end
 		end
 		return count
+	end
+
+	def getMembersByTime(given_date)
+		m = Array.new 
+		team_members.each do |member|
+			member.calendar_events.each do |event|
+				if(given_date.hour >= event.start.hour)&&(given_date.hour < event.end.hour)&&(given_date.day==event.start.day)
+					m.push(member.name)
+				end
+			end
+		end
+		available_members = Array.new
+		team_members.each do |member|
+			if not m.include?(member)
+				available_members.push(member.name)
+			end
+		end
+
+		m_string = ""
+		available_members.each do |name|
+			m_string = m_string + "\n"+name
+		end
+		return m_string
 	end
 
 	# def self.save_to_db(eventsArray)
